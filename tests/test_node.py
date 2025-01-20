a0476bdf497d6395b29abc76a66d21270de446b5
christian ambriz <christianambriz@christians-MacBook-Air.local>
2025-01-20 12:47:37 -0800

    write node class and tests class

---
+++ tests/test_node.py
@@ -0,0 +1,15 @@
+import unittest
+from models.Node import Node
+
+class TestNode(unittest.TestCase):
+    def test_node_creation(self):
+        node = Node(10)
+        self.assertEqual(node.value, 10) 
+
+    def test_node_update(self):
+        node = Node(5)
+        node.value = 20
+        self.assertEqual(node.value, 20)  
+
+if __name__ == '__main__':
+    unittest.main()
